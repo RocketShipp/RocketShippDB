2d904ad4dbdfdb7c94a9d121dfbe385752cd56db
Austin Shipp <austinshipp@yahoo.com>
2017-04-05 22:23:35 -0500

    Movie item model

--- src/models/ListModel.js
+++ src/models/ListModel.js
@@ -5,7 +5,40 @@ const ListSchema = new Schema({
     type: Schema.Types.ObjectId,
     requried: true
   },
-  items: [{}]
+  items: [{
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    originalTitle: {
+      type: String,
+      required: true
+    },
+    overview: {
+      type: String,
+      required: true
+    },
+    releaseDate: {
+      type: String,
+      required: true
+    },
+    voteAverage: {
+      type: Number,
+      required: true
+    },
+    voteCount: {
+      type: Number,
+      required: true
+    },
+    posterPath: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 export default mongoose.model('List', ListSchema);
